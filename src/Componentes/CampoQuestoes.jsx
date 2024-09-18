b9e2b69e3309bcabaa16b385b7782d2be47e00de
GabrielSantos9 <gabrielsantos9.programador@gmail.com>
2024-09-17 23:12:46 -0300

    Tela dos Questionamentos em desenvolvimento.

--- src/Componentes/CampoQuestoes.jsx
+++ src/Componentes/CampoQuestoes.jsx
@@ -1,12 +1,60 @@
 import React from "react";
-import { Fieldset, Legend } from "../Estilizações/ECampoQuestoes"
+import { Questao, FormQuestao, BotaoProximo } from "../Estilizações/ECampoQuestoes";
+
+const perguntas = [
+  {
+    pergunta: 'Qual método é utilizado para criar componentes?',
+    options: [
+      'React.makeComponent()',
+      'React.createComponent()',
+      'React.createElement()',
+    ],
+    resposta: 'React.createElement()',
+    id: 'p1',
+  },
+  {
+    pergunta: 'Como importamos um componente externo?',
+    options: [
+      'import Component from "./Component"',
+      'require("./Component")',
+      'import "./Component"',
+    ],
+    resposta: 'import Component from "./Component"',
+    id: 'p2',
+  },
+  {
+    pergunta: 'Qual hook não é nativo?',
+    options: ['useEffect()', 'useFetch()', 'useCallback()'],
+    resposta: 'useFetch()',
+    id: 'p3',
+  },
+  {
+    pergunta: 'Qual palavra deve ser utilizada para criarmos um hook?',
+    options: ['set', 'get', 'use'],
+    resposta: 'use',
+    id: 'p4',
+  },
+];
+
 
 const CampoQuestoes = () => {
+  const [respostas, setRespostas] = React.useState({
+    p1: '',
+    p2: '',
+    p3: '',
+    p4: '',
+  })
+  
   return (
-    <Fieldset>
-      <Legend>Teste</Legend>
-    </Fieldset>
+    <>
+      <FormQuestao>
+        {perguntas.map((pergunta) => (
+          <Questao {...pergunta}/>
+        ))}
+        <BotaoProximo>próximo</BotaoProximo>
+      </FormQuestao>
+    </>
   );
-}
+};
 
 export default CampoQuestoes;
--- src/Estilizações/ECampoQuestoes.jsx
+++ src/Estilizações/ECampoQuestoes.jsx
@@ -1,11 +1,58 @@
 import styled from "styled-components";
 
+export const FormQuestao = styled.form`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export const Fieldset = styled.fieldset`
-  border: 2px solid #ccc;
-  padding: 16px;
+  border: 2px solid rgba(31, 31, 31, 62);
   border-radius: 8px;
+  width: 800px;
+  height: 314px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 export const Legend = styled.legend`
   font-weight: bold;
+  font-size: 23px;
+  color: white;
+  margin-left: 30px;
+  padding-left: 15px;
+  padding-right: 20px;
 `;
+
+export const BotaoProximo = styled.button`
+  width: 220px;
+  height: 40px;
+  background-color: #8000ff;
+  border-radius: 8px;
+
+  font-size: 18px;
+  font-weight: 600;
+  color: white;
+  text-transform: uppercase;
+  margin-top: 10px;
+`;
+
+import React from "react";
+
+export const Questao = ({pergunta, options, id}) => {
+  return (
+      <Fieldset>
+        <Legend>{pergunta}</Legend>
+        {options.map((option) => (
+          <label key={option} style={{marginBottom: '1rem', margin:'flex'}}>
+            <input type="radio" value={option} />
+          </label>
+        ))}
+      </Fieldset>
+  );
+};
+
+export default Questao;
