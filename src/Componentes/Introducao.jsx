71c3e5acbe71663e2ec65ac5350e1d7e47d938c6
GabrielSantos9 <gabrielsantos9.programador@gmail.com>
2024-10-04 20:49:50 -0300

    Commit de conflito

--- src/Componentes/Introducao.jsx
+++ src/Componentes/Introducao.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
+  import React, { useState } from "react";
 
-import {
-  Intro,
-  Titulo,
-  Paragrafo,
-  CorReact,
-  ExemploNome,
-} from '../Estilizações/EIntroducao';
+  import {
+    Intro,
+    Titulo,
+    Paragrafo,
+    CorReact,
+    ExemploNome,
+  } from "../Estilizações/EIntroducao";
 
-import {
-  CampoNome,
-  LabelNome,
-  SvgNome,
-  BotaoProximo,
-  PreencherInput,
-} from '../Estilizações/ECampoNome';
+  import {
+    CampoNome,
+    LabelNome,
+    SvgNome,
+    BotaoProximo,
+    PreencherInput,
+  } from "../Estilizações/ECampoNome";
 
+<<<<<<< Updated upstream
 const Introducao = ({ AmarzenaNome }) => {
   const [StorageName, setStorageName] = React.useState('');
   
@@ -52,5 +53,46 @@ const Introducao = ({ AmarzenaNome }) => {
     </>
   );
 };
+=======
+  const Introducao = ({ AmarzenaNome }) => {
+    const [inputValue, setInputValue] = useState("");
 
-export default Introducao;
+    const handleInputChange = (e) => {
+      setInputValue(e.target.value);
+      console.log("Estado atual:", inputValue);
+    };
+>>>>>>> Stashed changes
+
+    return (
+      <>
+        <Intro>
+          <Titulo>
+            {" "}
+            Olá, seja bem vindo no meu primeiro projeto utilizando{" "}
+            <CorReact>React</CorReact>!
+          </Titulo>
+          <Paragrafo>
+            Antes de começar alguns questionamentos básicos sobre o React, será
+            necessário dizer o seu <ExemploNome>Nome</ExemploNome>!
+          </Paragrafo>
+        </Intro>
+        <CampoNome>
+          <SvgNome />
+          <LabelNome htmlFor="nome">Seu nome</LabelNome>
+          <PreencherInput
+            id="nome"
+            type="text"
+            name="nome"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <BotaoProximo type="button" onClick={AmarzenaNome}>
+            próximo
+          </BotaoProximo>
+          <p>Valor armazenado: {inputValue}</p>
+        </CampoNome>
+      </>
+    );
+  };
+
+  export default Introducao;
